9e8ccef3d6937fd1c5ed3134fca15bf159aca28e
mohamadfakir <113498626+mohamadfakir@users.noreply.github.com>
2022-12-02 11:34:44 +0530

    Add Command Line Argument

--- src/day1hw/commandline.java
+++ src/day1hw/commandline.java
@@ -1,6 +1,6 @@
 package day1hw;
 
-    public class commandline {
+    public class CommandLineArgument {
         public static void main (String[]arg){
             System.out.println("Your first argument is:"+arg[0]);
 
